d4e12a1b5c4795819db33ee80f9ecc55d469af1c
owencahyadi <owenchristian123@gmail.com>
2026-09-09 15:08:41 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Api\StoreController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\SupplierItemController;
-use App\Http\Controllers\Api\OperationalNoteController;
+use App\Http\Controllers\OperationalNoteController;
 
 // ---------------------------------------------------------
 // Rute Publik (Tidak perlu token untuk akses)
